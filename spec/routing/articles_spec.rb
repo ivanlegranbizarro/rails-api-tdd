679068389874e21c6985640df306abc9519d4676
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2021-09-06 06:13:02 +0200

    Routing Test

---
+++ spec/routing/articles_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe '/articles routes' do
+  it 'routes to articles#index' do
+    aggregate_failures do
+      expect(get 'articles').to route_to('articles#index')
+    end
+  end
+end
